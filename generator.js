7e5ef2a2c165003e5a459688976575b1fd6f76cf
Pavlo Rudyi <paulcarroty@users.noreply.github.com>
2018-07-23 17:27:20 +0000

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,29 +1,40 @@
-const fruitList = ['Orange','Apple', 'Mango'];
+const fruitList = ['Banana', 'Orange'];
 
-// create our looping generator
-function* loop(arr) {
-  for (const fruit of fruitList) {
-    yield `I like to eat ${fruit}`;
-  }
+const fruitList2 = ['Cherry', 'Mango'];
+
+// looping generator over two arrays
+function* loop() {
+    for (const fruit of fruitList) {
+        yield `I like to eat ${fruit} from the first list`;
+    }
+
+    for (const fruit of fruitList2) {
+        yield `I don't like to eat ${fruit} from the second list`;
+    }
 }
 
+let generator = loop();
+console.log(generator.next())
+console.log(generator.next())
+console.log(generator.next())
+console.log(generator.next())
 
-const fruitGenerator = loop(fruitList);
-fruitGenerator.next();
-// Object { value: "I like to eat Orange", done: false }
-fruitGenerator.next();
-// Object { value: "I like to eat Apple", done: false }
-fruitGenerator.next().value;
-// "I like to eat Mango"
+// { value: 'I like to eat Banana from the first list',
+  done: false }
+// { value: 'I like to eat Orange from the first list',
+  done: false }
+// { value: 'I don\'t like to eat Cherry from the second list',
+  done: false }
+// { value: 'I don\'t like to eat Mango from the second list',
+  done: false }
 
 
-
-// .return() for finishing the genarator
-fruitGenerator.return();
+// .return() method will finish the generator
+generator.return();
 // Object { value: undefined, done: true }
 
 
-// catching errors
+// handle errors
 fruitGenerator.throw("ooops")
 // Error: ooops
 // Object { value: undefined, done: true }
